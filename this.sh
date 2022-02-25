95fc6bf5c95ff1d50a23eaf1dab653d6f40e4f5a
Alx <aabcad@gmail.com>
2022-02-25 14:28:13 +0300

    Initial

---
+++ this.sh
@@ -0,0 +1,1 @@
+LD_LIBRARY_PATH=/usr/local/MATLAB/MATLAB_Runtime/v97/runtime/glnxa64:/usr/local/MATLAB/MATLAB_Runtime/v97/bin/glnxa64:/usr/local/MATLAB/MATLAB_Runtime/v97/sys/os/glnxa64:/usr/local/MATLAB/MATLAB_Runtime/v97/extern/bin/glnxa64 ./this $1
